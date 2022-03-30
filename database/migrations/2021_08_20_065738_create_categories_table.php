7b27ba721378885abca5c6f2fe4aa318a4cc4948
Nhật Nguyễn Tiến <kaotuong113@gmail.com>
2022-03-30 21:17:58 +0700

    edit migration cate

--- database/migrations/2021_08_20_065738_create_categories_table.php
+++ database/migrations/2021_08_20_065738_create_categories_table.php
@@ -14,7 +14,7 @@ class CreateCategoriesTable extends Migration
     public function up()
     {
         Schema::create('categories', function (Blueprint $table) {
-            $table->id('id');
+            $table->increments('id');
             $table->string('category_name');
             $table->timestamps();
             $table->integer('createBy')->unsigned()->default(1);
